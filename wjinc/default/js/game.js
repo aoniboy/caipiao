e4f318a0b00f1a0d8de688463c33ee0862290471
sht <you@example.com>
2018-04-28 12:39:16 +0800

    1111

--- wjinc/default/js/game.js
+++ wjinc/default/js/game.js
@@ -520,11 +520,11 @@ var game = {
                     game.countdown(data.data.actionNo.difftime);
                     game.global.gametimer = null
                     if(data.data.kjNo){
-                        console.log(1111111);
+                        console.log(111);
                         clearInterval(game.global.gametimer);
                         $(".gameo_num").html(data.data.kjNo);
                     }else{
-                        console.log(22222222);
+                        console.log(222);
                     }
                 }else{
                     $(".hint_pop .hint_cont").text(data.msg);
@@ -546,6 +546,7 @@ var game = {
                 game.countdown(data.data.actionNo.difftime);
                 game.global.gametimer = null;
                 if(!data.data.kjNo){
+                    if(!game.is_false){
                         game.global.gametimer =setInterval(function(){
                                                     for(var i=0;i<$(".gameo_num span").length;i++){
                                                         $(".gameo_num span").eq(i).text(game.randomNum())
@@ -558,6 +559,7 @@ var game = {
                     game.timekjinfo();
 
                 }else{ 
+
                 	//clearInterval(game.global.gametimer);
                     $(".gameo_num").html(data.data.kjNo);
                 }
