5824b135d281de6aa3fcd960a50fef4e509f668b
sht <you@example.com>
2018-05-04 18:35:24 +0800

    123

--- wjinc/default/newsafe/dl_game.php
+++ wjinc/default/newsafe/dl_game.php
@@ -95,10 +95,11 @@
             </div>
         </div>
         </div>
-    <div class="myp_table" style="width:800px">
-        
+    <div class="table_scroll">
+        <div class="myp_table" style="width:800px">
+            
+        </div>
     </div>
- 
 </div>
 
 <script src="/wjinc/default/js/common.js"></script>
