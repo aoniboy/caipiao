5aafdddaae7fc8398b1ad096c36f257e7b03df14
smz <smz@example.com>
2018-05-05 18:19:05 +0800

    modify

--- wjinc/default/newindex/game-list.php
+++ wjinc/default/newindex/game-list.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>开奖</title>
+    <title>游戏</title>
     <link rel="stylesheet" type="text/css" href="/wjinc/default/css/style.css">
     <link rel="stylesheet" type="text/css" href="/wjinc/default/css/font.css">
     <script src="/wjinc/default/js/jquery.min.js"></script>
