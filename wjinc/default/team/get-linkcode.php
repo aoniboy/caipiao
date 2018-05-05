3de435e896c2a39cab3777faea1610d4318198a7
sht <you@example.com>
2018-05-05 23:28:04 +0800

    123

--- wjinc/default/team/get-linkcode.php
+++ wjinc/default/team/get-linkcode.php
@@ -20,26 +20,26 @@
 ?><div>
 	<ul class="myi_list dla_list">
 		<li class="clearfix">
-            <div class="fl myw " style="width:40%">上级会员：</div>
+            <div class="fl myw f24" style="width:40%">上级会员：</div>
             <input  class="col67 fl f22" style="width:55%" readonly value="<?=$parentData['username']?>">
         </li>
         <li class="clearfix">
-            <div class="fl myw " style="width:40%">返点：</div>
+            <div class="fl myw f24" style="width:40%">返点：</div>
             <input class="col67 fl i5" style="width:20%"  type="text" name="fanDian" value="<?=$linkData['fanDian']?>" max="<?=$parentData['fanDian']?>" min="0" fanDianDiff=<?=$this->settings['fanDianDiff']?> >
             <span class="fl">0—<?=$parentData['fanDian']?>%</span>
         </li>
         <li class="clearfix">
-            <div class="fl myw " style="width:40%">不定位返点：</div>
+            <div class="fl myw f24" style="width:40%">不定位返点：</div>
             <input class="col67 fl i6" style="width:20%" name="fanDianBdw" value="<?=$linkData['fanDianBdw']?>" max="<?=$parentData['fanDianBdw']?>" min="0">
             <span class="fl">0—<?=$parentData['fanDianBdw']?>%</span>
         </li>
         <li class="clearfix">
-            <div class="fl myw " style="width:40%">注册推广链接：</div>
+            <div class="fl myw f24" style="width:40%">注册推广链接：</div>
             <input  class="col67 fl f22" style="width:55%" readonly id="adv-url" readonly="readonly" value="http://<?=$_SERVER['HTTP_HOST']?>/index.php/user/r/<?=$key?>" >
         </li>
         <li class="clearfix">
-            <div class="fl myw " style="width:40%"></div>
-            <input  class="col67 fl f22" style="width:55%;border: 1px solid blue; font-weight: 700" readonly id="clip_button" onClick="copyNum()" value="点击复制">
+            <div class="fl myw f24" style="width:40%"> &nbsp</div>
+            <input  class="col67 fl f30" style="width:55%;border: 1px solid blue; text-align: center; font-weight: 700" readonly id="clip_button" onClick="copyNum()" value="点击复制">
         </li>
     </ul>
 	
