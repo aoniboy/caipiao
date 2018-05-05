24cb18a03dc8938083840bfeb53d2243dfa1037a
sht <you@example.com>
2018-05-05 23:00:18 +0800

    123

--- wjinc/default/team/update-link.php
+++ wjinc/default/team/update-link.php
@@ -14,28 +14,21 @@
 <div>
 <form action="/index.php/team/linkUpdateed" target="ajax" method="post" call="linkDataSubmitCode" onajax="linkDataBeforeSubmitCode" dataType="html">
 	<input type="hidden" name="lid" value="<?=$args[0]?>"/>
-
-	<table cellpadding="2" cellspacing="2" class="popupModal">
-		
-	<!--	<tr>
-			<td class="title">用户名：</td>
-			<td><input type="text" name="username" readonly="readonly" value="<?=$parentData['username']?>"/></td>
-		</tr>-->
-		
-		<tr>
-			<td class="title">返点：</td>
-			<td><input type="text" name="fanDian" value="<?=$linkData['fanDian']?>" max="<?=$parentData['fanDian']?>" min="0" fanDianDiff=<?=$this->settings['fanDianDiff']?> >%&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#999">0—<?=$parentData['fanDian']?>%</span></td>
-		</tr>
-		<tr>
-			<td class="title">不定返点：</td>
-			<td><input type="text" name="fanDianBdw" value="<?=$linkData['fanDianBdw']?>" max="<?=$parentData['fanDianBdw']?>" min="0"/>%&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#999">0—<?=$parentData['fanDianBdw']?>%</span></td>
-		</tr>
-
-        <tr>
-        	<td class="title">加入时间：</td>
-			<td><?=date("Y-m-d H:i:s",$linkData['regTime'])?></td>
-        </tr>
-        
-	</table>
+    <ul class="myi_list dla_list">
+        <li class="clearfix">
+            <div class="fl myw ">返点：</div>
+            <input class="col67 fl i5"  type="text" name="fanDian" value="<?=$linkData['fanDian']?>" max="<?=$parentData['fanDian']?>" min="0" fanDianDiff=<?=$this->settings['fanDianDiff']?> >
+            <span class="">0—<?=$parentData['fanDian']?>%</span>
+        </li>
+        <li class="clearfix">
+            <div class="fl myw ">不定位返点：</div>
+            <input class="col67 fl i6" name="fanDianBdw" value="<?=$linkData['fanDianBdw']?>" max="<?=$parentData['fanDianBdw']?>" min="0">
+            <span class="fl">0—<?=$parentData['fanDianBdw']?>%</span>
+        </li>
+        <li class="clearfix">
+            <div class="fl myw ">加入时间</div>
+            <input  class="col67 fl" readonly value="<?=date("Y-m-d H:i:s",$linkData['regTime'])?>">
+        </li>
+    </ul>
 </form>
 </div>
