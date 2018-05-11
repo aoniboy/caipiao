6bba488f2efb3636179564c43bf738c075273dc2
sht <you@example.com>
2018-05-11 21:14:00 +0800

    123

--- wjinc/default/cash/recharge-copy.php
+++ wjinc/default/cash/recharge-copy.php
@@ -3,12 +3,41 @@ $mBankId=$_POST['mBankId'];
 $sql="select mb.*, b.name bankName, b.logo bankLogo, b.home bankHome,mb.qrcode from {$this->prename}sysadmin_bank mb, {$this->prename}bank_list b where mb.id=$mBankId and b.isDelete=0 and mb.bankId=b.id";
 
 $memberBank=$this->getRow($sql);
-print_r($memberBank);
 $set=$this->getSystemSettings();
 
 ?>
-
-<table width="100%" border="0" cellspacing="1" cellpadding="3" class='table_b'>
+         <div class="title_top tc"><a href="javascript:history.back(-1)" class="iconfont icon-xiangzuojiantou iconback"></a>充值信息</div>
+    <div class="f24 myt_text">
+              充值说明：<br>
+        1.扫二维码支付，需要在<b style="display:inline;color:#ff2525;">备注输入充值帐号信息</b>，<b style="display:inline;color:#ff2525;">如：充值帐号：123456。</b><br>
+        2.转账后如<b style="display:inline;color:#ff2525;">5分钟</b>未到账，请联系客服，告知您的充值编号和您的充值金额及你的充值账号。<br>
+        3.充值有效金额：<b style="display:inline;color:#ff2525;"><?=$set['rechargeMin1']?>元-<?=$set['rechargeMax1']?>元</b>。充值金额请输入非整数，以便平台更快速为您进行充值，谢谢！例如：充值<b style="display:inline;color:#ff2525;">2000.01</b>元。
+    </div>
+        <ul class="myi_list myt_list myt_list1">
+            <li class="clearfix rel">
+                <div class="fl myw">充值方式：</div>
+                <img class="col67 fl" src="/<?=$memberBank['bankLogo']?>" title="<?=$memberBank['bankName']?>">
+            </li>
+            <li class="clearfix rel">
+                <div class="fl myw">充值金额：</div>
+                <p class="col67 fl"><?=$_POST['amount']?></p>
+            </li>
+            <li class="clearfix rel" style="">
+                <span class="f20">*充值金额必须与网站填写金额一致方能到账！</span>
+            </li>
+            <li class="clearfix rel">
+                <div class="fl myw">充值编号：</div>
+                <p class="col67 fl"><?=$_POST['rechargeId']?></p>
+            </li>
+            <li class="clearfix rel">
+               *网银充值请务必将此编号填写到汇款“附言”里，每个充值编号仅用于一笔充值！
+            </li>
+            <li class="clearfix rel tc" style="background:#eee;">
+               <a style="display: block;"> href="/index/help/<?=$memberBank['bankId']==2?2:1?>">支付帮助</a>
+            </li>
+        </ul>
+        <img style="width:80%;display: block;margin:.1rem auto;" src="http://www.g19u.com/<?=$memberBank['qrcode']?>">
+<!-- <table width="100%" border="0" cellspacing="1" cellpadding="3" class='table_b'>
     <tr class='table_b_th'>
       <td align="left" style="font-weight:bold;padding-left:10px;" colspan=2>充值信息</td> 
     </tr>
@@ -51,4 +80,4 @@ $set=$this->getSystemSettings();
         <dd>3.充值有效金额：<b style="display:inline;color:#FF0000;"><?=$set['rechargeMin1']?>元-<?=$set['rechargeMax1']?>元</b>。充值金额请输入非整数，以便平台更快速为您进行充值，谢谢！例如：充值<b style="display:inline;color:#FF0000;">2000.01</b>元。</dd>
     </dl>
 </div>
-</table>
+</table> -->
--- wjinc/default/newsafe/my_recharge.php
+++ wjinc/default/newsafe/my_recharge.php
@@ -24,58 +24,62 @@ $set=$this->getSystemSettings();
 </style>
 
 <div class="wrap_box">
-    <div class="title_top tc"><a href="javascript:history.back(-1)" class="iconfont icon-xiangzuojiantou iconback"></a>充值</div>
-    <div class="f24 myt_text">1、<b style="display:inline;color:#ff2525;">在线网银充值</b>则为<b style="display:inline;color:#ff2525;">24小时不间断</b>；<br>
-              2、<b style="display:inline;color:#ff2525;">微信、支付宝</b>每天的充值处理时间为：<b style="display:inline;color:#ff2525;">10:00-17：00，19：00-24：00</b>，填写充值金额，点击[下一步]后，将有详细的文字说明 ；<br>
-              3、充值后，<b style="display:inline;color:#ff2525;">请手动刷新&nbsp&nbsp</b>  你的余额及查看相关账变信息，若超过5分钟未加金额，请与在线客服联系。
-    </div>
-
-    <form class="myt_form">
-        <?php
-            $sql="select * from {$this->prename}bank_list b, {$this->prename}sysadmin_bank m where m.admin=1 and m.enable=1 and b.isDelete=0 and b.id=m.bankId";
-            $banks=$this->getRows($sql);
-                    
-        if($banks){?>
-        <ul class="myi_list myt_list myt_list1">
-            <li class="clearfix rel">
-                <div class="fl myw">充值方式：</div>
-
-                <div class="fl ol67 pay_fs">
-
-                </div>
-            </li>
-            <li class="clearfix rel flex cz_online">
-                
-        
-                    <?php
-                        if($banks) foreach($banks as $bank){
-                    ?>
-                    <div class="fx">
-                        <label><input class="pay_checked" value="<?=$bank['id']?>" type="radio" bankid="<?=$bank['bankId']?>" name="mBankId" data-bank='<?=json_encode($bank)?>' style="width:auto;-webkit-appearance:radio;" /><img style="display: inline; width:80%" src="/<?=$bank['logo']?>"></label>
-                    </div>
-                    <?php } ?>
-           
-            </li>
-            <li class="clearfix rel">
-                <div class="fl myw">充值金额：</div>
-                <input class="col67 fl n2" type="tel" style="width:1rem;" name="amount"  value="<?=preg_replace('/^.*(\w{4})$/', '***\1', $bank['account'])?>">
-                <span class="f20 cz_pos" style="pointer-events:none" id="rechargemsg">(单笔限额 最低： <b style="color:#ff2525"><?=$set['rechargeMin']?></b>  元，最高：  <b style="color:#ff2525"><?=$set['rechargeMax']?></b>  元)</span>
-            </li>
-            <li class="clearfix rel">
-                <div class="fl myw">验证码：</div>
-                <input class="col67 fl n3" type="text"  name="vcode" value="<?=preg_replace('/^(\w).*$/', '\1**', $bank['username'])?>">
-                <b class="yzmNum"><img width="80" height="30" border="0" style="cursor:pointer;margin-bottom:0px;" id="vcode" alt="看不清？点击更换" align="absmiddle" src="/index.php/user/vcode/<?=$this->time?>" title="看不清楚，换一张图片" onclick="this.src='/index.php/user/vcode/'+(new Date()).getTime()"/></b>
-            </li>
-        </ul>
-        <input class="myt_btn tc chongzhi_btn" type="button" value="下一步">
-        <?php }else{ ?>
-        <div style=" margin-top:30px; text-align:center;color:#F00;">
-            充值暂停！
+    <div class="pay_box1">
+        <div class="title_top tc"><a href="javascript:history.back(-1)" class="iconfont icon-xiangzuojiantou iconback"></a>充值</div>
+        <div class="f24 myt_text">1、<b style="display:inline;color:#ff2525;">在线网银充值</b>则为<b style="display:inline;color:#ff2525;">24小时不间断</b>；<br>
+                  2、<b style="display:inline;color:#ff2525;">微信、支付宝</b>每天的充值处理时间为：<b style="display:inline;color:#ff2525;">10:00-17：00，19：00-24：00</b>，填写充值金额，点击[下一步]后，将有详细的文字说明 ；<br>
+                  3、充值后，<b style="display:inline;color:#ff2525;">请手动刷新&nbsp&nbsp</b>  你的余额及查看相关账变信息，若超过5分钟未加金额，请与在线客服联系。
         </div>
-        <?php }?>
-    </form>
-    <div class="cz_html"></div>
-        <div class="hint_pop hide">
+
+        <form class="myt_form">
+            <?php
+                $sql="select * from {$this->prename}bank_list b, {$this->prename}sysadmin_bank m where m.admin=1 and m.enable=1 and b.isDelete=0 and b.id=m.bankId";
+                $banks=$this->getRows($sql);
+                        
+            if($banks){?>
+            <ul class="myi_list myt_list myt_list1">
+                <li class="clearfix rel">
+                    <div class="fl myw">充值方式：</div>
+
+                    <div class="fl ol67 pay_fs">
+
+                    </div>
+                </li>
+                <li class="clearfix rel flex cz_online">
+                    
+            
+                        <?php
+                            if($banks) foreach($banks as $bank){
+                        ?>
+                        <div class="fx">
+                            <label><input class="pay_checked" value="<?=$bank['id']?>" type="radio" bankid="<?=$bank['bankId']?>" name="mBankId" data-bank='<?=json_encode($bank)?>' style="width:auto;-webkit-appearance:radio;" /><img style="display: inline; width:80%" src="/<?=$bank['logo']?>"></label>
+                        </div>
+                        <?php } ?>
+               
+                </li>
+                <li class="clearfix rel">
+                    <div class="fl myw">充值金额：</div>
+                    <input class="col67 fl n2" type="tel" style="width:1rem;" name="amount"  value="<?=preg_replace('/^.*(\w{4})$/', '***\1', $bank['account'])?>">
+                    <span class="f20 cz_pos" style="pointer-events:none" id="rechargemsg">(单笔限额 最低： <b style="color:#ff2525"><?=$set['rechargeMin']?></b>  元，最高：  <b style="color:#ff2525"><?=$set['rechargeMax']?></b>  元)</span>
+                </li>
+                <li class="clearfix rel">
+                    <div class="fl myw">验证码：</div>
+                    <input class="col67 fl n3" type="text"  name="vcode" value="<?=preg_replace('/^(\w).*$/', '\1**', $bank['username'])?>">
+                    <b class="yzmNum"><img width="80" height="30" border="0" style="cursor:pointer;margin-bottom:0px;" id="vcode" alt="看不清？点击更换" align="absmiddle" src="/index.php/user/vcode/<?=$this->time?>" title="看不清楚，换一张图片" onclick="this.src='/index.php/user/vcode/'+(new Date()).getTime()"/></b>
+                </li>
+            </ul>
+            <input class="myt_btn tc chongzhi_btn" type="button" value="下一步">
+            <?php }else{ ?>
+            <div style=" margin-top:30px; text-align:center;color:#F00;">
+                充值暂停！
+            </div>
+            <?php }?>
+        </form>
+    </div>
+    <div class="pay_box2">
+        
+    </div>
+    <div class="hint_pop hide">
         <div class="gameo_mask"></div>
         <div class="hint_con">
             <div class="hint_title f32 tc hint_titles">错误提示</div>
@@ -123,7 +127,8 @@ $set=$this->getSystemSettings();
         }
         $.post('/index.php/cash/inRecharge',$('.myt_form').serialize(), function(res){
             if(!res.code){
-                $(".cz_html").html(res.data); 
+                $(".pay_box2").html(res.data); 
+                $(".pay_box1").hide();
 
             }else{
                 $(".hint_pop1").show();
