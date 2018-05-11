b9b1c02cb710349f292258f50a8006a52e7c15b8
sht <you@example.com>
2018-05-11 20:27:35 +0800

    123

--- wjinc/default/cash/recharge-copy.php
+++ wjinc/default/cash/recharge-copy.php
@@ -1,5 +1,5 @@
 <?php
-$mBankId=$get[0]['mBankId'];
+$mBankId=$GET[0]['mBankId'];
 $sql="select mb.*, b.name bankName, b.logo bankLogo, b.home bankHome,mb.qrcode from {$this->prename}sysadmin_bank mb, {$this->prename}bank_list b where mb.id=$mBankId and b.isDelete=0 and mb.bankId=b.id";
 
 $memberBank=$this->getRow($sql);
